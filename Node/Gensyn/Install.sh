81c494b89602656aca0f6291ab9fc25223c6d842
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2025-04-03 16:02:49 +0300

    Update Install.sh

--- Node/Gensyn/Install.sh
+++ Node/Gensyn/Install.sh
@@ -7,6 +7,4 @@ curl -sSL https://raw.githubusercontent.com/zunxbt/installation/main/node.sh | b
 sleep 3
 sudo apt update && sudo apt install -y python3 python3-venv python3-pip curl screen git yarn && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt update && sudo apt install -y yarn
 sleep 3
-rm -rf rl-swarm && git clone https://github.com/zunxbt/rl-swarm.git 
-sleep 3
-cd /root/rl-swarm/
+rm -rf rl-swarm && git clone https://github.com/zunxbt/rl-swarm.git
