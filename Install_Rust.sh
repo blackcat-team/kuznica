8c61568aa6a914bdd99f70b3ddee634c6bee249e
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2025-04-13 00:22:08 +0300

    Update Install_Rust.sh

--- Install_Rust.sh
+++ Install_Rust.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update
-sudo apt install curl make clang pkg-config libssl-dev build-essential git mc jq unzip wget -y
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source $HOME/.cargo/env
-sleep 1
+set -e
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
+rustup update
+sleep 2
