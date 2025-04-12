e966c4f4d05abb450fac16895fac3ecbe0a39893
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2025-04-12 23:56:18 +0300

    Update Install.sh

--- Node/BITZ /Install.sh
+++ Node/BITZ /Install.sh
@@ -6,25 +6,25 @@ echo "*******************************************************"
 green() {
     echo -e "\e[32m$1\e[0m"
 }
-green  "Устанавливаем необходимое ПО"
+green  "Начинаю установку ноды"
 green  "Обновляю пакеты, пожалуйста подождите....."
 bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/main%20install) &>/dev/null
 green  "Обновление успешно завершено."
 green  "Устанавливаю Rust, пожалуйста, подождите..."
 bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Install_Rust.sh) &>/dev/null
-green  "Необходимое ПО установлено, продолжаем установку ноды"
+green "Rust установлен, продалжаю установку ноды"
 sudo apt install -y build-essential pkg-config libssl-dev clang
-green  "Устанавливаем Solana CLI"
+green  "Устанавливаем Solana CLI..."
 bash <(curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Install_Solana_CLI) &>/dev/null
 sleep 1
-export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
-sleep 1
-echo 'export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"' >> ~/.bashrc
-sleep 1
+green "CLI установлен, продолжаем установку ноды"
 solana-keygen new --no-passphrase
 sleep 5
-green  "!!!Обязательно СОХРАНИТЕ seed фразу выше к себе на компьютер!!!"
-sleep 15
+green "Начинаю генерацию приватного ключа..."
+sleep 5
+green "Ваш приватный ключ:"
 cat ~/.config/solana/id.json
+green  "!!!Обязательно СОХРАНИТЕ приватный ключ к себе на компьютер!!!"
+sleep 20
 cargo install bitz
 solana config set --url https://mainnetbeta-rpc.eclipse.xyz/
