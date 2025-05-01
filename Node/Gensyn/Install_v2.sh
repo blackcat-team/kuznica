33484a3246bb6487cc22539fcd977c53df0db290
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2025-05-01 10:54:31 +0300

    Create Install_v2.sh

---
+++ Node/Gensyn/Install_v2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "*******************************************************"
+curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/kuznica_logo.sh | bash
+echo "*******************************************************"
+sleep 3
+curl -sSL https://raw.githubusercontent.com/zunxbt/installation/main/node.sh | bash
+sleep 3
+sudo apt update && sudo apt install -y python3 python3-venv python3-pip curl screen git yarn && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt update && sudo apt install -y yarn
+sleep 3
+rm -rf rl-swarm && git clone https://github.com/gensyn-ai/rl-swarm.git
