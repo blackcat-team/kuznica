19381a8372462ac9b821e903493ce55975ddff5e
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2025-05-05 16:51:10 +0300

    Update Install.sh

--- Node/Aztec/Install.sh
+++ Node/Aztec/Install.sh
@@ -79,19 +79,19 @@ read -p "$(cyan 'Введите адрес кошелька, приватный 
 
 green "Вносим данные в конфигурацию ноды..."
 
-cat > $HOME/start_aztec_node.sh << EOL
+cat > $HOME/start_aztec_node.sh << EOF
 #!/bin/bash
 export PATH=\$PATH:\$HOME/.aztec/bin
 aztec start --node --archiver --sequencer \\
   --network alpha-testnet \\
   --port 8080 \\
-  --l1-rpc-urls $L1_RPC_URL \\
-  --l1-consensus-host-urls $L1_CONSENSUS_URL \\
-  --sequencer.validatorPrivateKey $VALIDATOR_PRIVATE_KEY \\
-  --sequencer.coinbase $ETH_ADDRESS \\
-  --p2p.p2pIp $IP \\
+  --l1-rpc-urls \$L1_RPC_URL \\
+  --l1-consensus-host-urls \$L1_CONSENSUS_URL \\
+  --sequencer.validatorPrivateKey \$VALIDATOR_PRIVATE_KEY \\
+  --sequencer.coinbase \$ETH_ADDRESS \\
+  --p2p.p2pIp \$IP \\
   --p2p.maxTxPoolSize 1000000000
-EOL
+EOF
 
 chmod +x "$HOME/start_aztec_node.sh"
 
