d8a527cbf3c0ac8f88891766c1e33e4d19e41d7a
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2025-06-17 11:56:29 +0300

    Update Install.sh

--- Node/Gensyn/Install.sh
+++ Node/Gensyn/Install.sh
@@ -4,7 +4,35 @@ curl -s https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/
 echo "*******************************************************"
 sleep 3
 curl -sSL https://raw.githubusercontent.com/zunxbt/installation/main/node.sh | bash
-sleep 3
-sudo apt update && sudo apt install -y python3 python3-venv python3-pip curl screen git yarn && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt update && sudo apt install -y yarn
-sleep 3
-rm -rf rl-swarm && git clone https://github.com/zunxbt/rl-swarm.git
+sleep 1
+sudo apt install -y python3 python3-pip python3-venv git
+sleep 1
+git clone https://github.com/gensyn-ai/rl-swarm.git
+sleep 1
+cd rl-swarm
+sleep 1
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+sudo apt install -y npm
+sudo npm install -g yarn
+sudo npm install -g n
+sudo n lts
+n 20.18.0
+
+CONFIG_PATH="/root/rl-swarm/hivemind_exp/configs/gpu/grpo-qwen-2.5-1.5b-deepseek-r1.yaml"
+REMOTE_URL="https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Node/Gensyn/grpo-qwen-2.5-1.5b-deepseek-r1.yaml"
+
+echo "[INFO] Removing old config if it exists..."
+rm -f "$CONFIG_PATH"
+
+echo "[INFO] Downloading new config from GitHub..."
+curl -fsSL "$REMOTE_URL" -o "$CONFIG_PATH"
+
+if [ $? -eq 0 ]; then
+  echo "[SUCCESS] Config updated at $CONFIG_PATH"
+else
+  echo "[ERROR] Failed to download config from $REMOTE_URL"
+  exit 1
+fi
+./run_rl_swarm.sh
