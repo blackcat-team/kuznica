4e52cd591f22c81f1255f129189d7d2ca3736392
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2025-06-26 08:48:19 +0300

    Update Update26.06.2025.sh

--- Node/Gensyn/Update26.06.2025.sh
+++ Node/Gensyn/Update26.06.2025.sh
@@ -7,20 +7,11 @@ echo "*******************************************************"
 # Завершаем старую screen-сессию gensyn, если она запущена
 screen -S gensyn -X quit 2>/dev/null
 
-# Запускаем новую screen-сессию в фоне с нужным набором команд
-screen -dmS gensyn bash -c '
-  echo "Запуск внутри screen..."
-  npm install -g yarn --force
-  yarn install
+npm install -g yarn --force
+yarn install
 
-  rm -rf rl-swarm
-  git clone https://github.com/gensyn-ai/rl-swarm
-  cd rl-swarm
+rm -rf rl-swarm
+git clone https://github.com/gensyn-ai/rl-swarm
+cd rl-swarm
 
-  python3 -m venv .venv && source .venv/bin/activate && ./run_rl_swarm.sh
-
-  exec bash
-'
-
-# Подключаемся к созданной screen-сессии (переключаем терминал в неё)
-screen -r gensyn
+python3 -m venv .venv && source .venv/bin/activate && ./run_rl_swarm.sh
