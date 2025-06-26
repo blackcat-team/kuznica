32ca38620dd04d49be10093df0fa7ddd082f39e3
BlackCat <62426860+blackcat-team@users.noreply.github.com>
2025-06-26 08:58:29 +0300

    Update Install.sh

--- Node/Gensyn/Install.sh
+++ Node/Gensyn/Install.sh
@@ -21,24 +21,4 @@ sudo npm install -g n
 sudo n lts
 n 20.18.0
 
-CONFIG_PATH="/root/rl-swarm/hivemind_exp/configs/gpu/grpo-qwen-2.5-1.5b-deepseek-r1.yaml"
-REMOTE_URL="https://raw.githubusercontent.com/blackcat-team/kuznica/main/Node/Gensyn/grpo-qwen-2.5-1.5b-deepseek-r1.yaml"
-
-echo "[INFO] Removing old config if it exists..."
-rm -f "$CONFIG_PATH"
-
-echo "[INFO] Downloading new config from GitHub..."
-curl -fsSL "$REMOTE_URL" -o "$CONFIG_PATH"
-
-if [ $? -eq 0 ]; then
-  echo "[SUCCESS] Config updated at $CONFIG_PATH"
-else
-  echo "[ERROR] Failed to download config from $REMOTE_URL"
-  exit 1
-fi
-TARGET_FILE="/root/rl-swarm/hivemind_exp/runner/grpo_runner.py"
-sed -i '/hivemind\.DHT/ {
-  s/ensure_bootstrap_success *= *True/ensure_bootstrap_success=False/
-  /ensure_bootstrap_success/! s|\(hivemind.DHT(start=True, startup_timeout=30, \)|\1ensure_bootstrap_success=False, |
-}' "$TARGET_FILE"
 ./run_rl_swarm.sh
